ac6cf8c22292862f805cd83090a83ec1936771b4
LGAD user <lgaduser@fhl-aidainnova-lgad1.desy.de>
2022-03-14 12:31:31 +0100

    updated read_CAEN_txt to read files and add triggerIDs from TLU

--- read_CAEN_txt.py
+++ read_CAEN_txt.py
@@ -1,33 +1,31 @@
 import ROOT
-import numpy as np
 from array import array
 import statistics
-
-
+import argparse
+#source root before running:  source /opt/root/bin/thisroot.sh 
+#written for ROOT with python 3.8, earlier python versions may not work (though likely will)
 
 def geti0(c, imax, pmax, cfd = 50., timeBin = 0.4):
   i0 = int(0)
   epsilon = 0.0000000001
   for i in range(imax):
-     if c[imax -i] < cfd/100.*pmax:
+     if c[imax -i] < float(cfd/100.)*pmax:
        i0 = imax -i
   delta = c[i0+1] - c[i0] +epsilon #voltage difference between successive points, epsilon added to avoid division by 0
   delta50 = (pmax*cfd/100. - c[i0]) #votlage difference between 50% mark
   deltai =   delta50/delta
-  i0 = float(i0) +deltai
+ # i0 = float(i0) +deltai # interpolation is a little buggy, fix this later
+  i0 = imax
   return i0
 
-
-  
-def read_single_caen(input, output,  iteration, timeBin = 0.4, ):
+def read_single_caen(inF, output,  iteration, polarity, timeBin = 0.4, ):
     #use arparser here...
     #hard-coded for now...
     outfile = ROOT.TFile(output, 'RECREATE')
     tree = ROOT.TTree('tree'+str( iteration),'tree'+str( iteration))
-
-    w, h = 1, 1024
-    c = array('f', [0.]*1024)#[][ [ 0 for y in range( 1024 ) ] for x in range( nch ) ]
-    t =  array('f', [0.]*1024)#[]
+    c = array('f', [0.]*1024)
+    temp = array('f', [0.]*1024)
+    t =  array('f', [0.]*1024)
     pmax = array('f', [0.])
     tmax = array('f', [0.])
     t0 = array('f', [0.])
@@ -38,9 +36,8 @@ def read_single_caen(input, output,  iteration, timeBin = 0.4, ):
     tree.Branch("pmax", pmax, "pmax/F")
     tree.Branch("tmax", tmax, "tmax/F")
     tree.Branch("t0", t0, "t0/F")
-  #  tree.Branch("trigID", trigID, "trigID/I")
 
-    f = open(input, 'r')
+    f = open(inF, 'r')
     lines = f.readlines()
 
     for i in range(1024):
@@ -48,17 +45,18 @@ def read_single_caen(input, output,  iteration, timeBin = 0.4, ):
     i = 0
     evt = 0
     for line in lines:
-      try:
-        float(line)
-      except:
-         # print(line)
-          continue
-      c[i] = float(line) *0.25 #*-1 #negative one to change pulse polarity, 0.25 for ADV to mV
+ #     try:   #try statement if headers are included, as of now they typically are not
+  #      float(line)
+  #    except:
+  #       # print(line)
+  #      continue
+      temp[i] = float(line) *0.25 *polarity#*-1 #negative one to change pulse polarity, 0.25 for ADV to mV
       i = i+1
       if i == 1024:
-       # print(c)
-        pedestal = statistics.mean(c[:50])
-        for j in range(1024): c[j]= c[j] - pedestal
+ #       print(c[-1])
+        pedestal = statistics.mean(temp[:50])
+        #print ("the pedestal is: "+str(pedestal))
+        for j in range(1024): c[j]= float(temp[j] - pedestal)
         pmax[0] = max(c)
         imax = c.index(pmax[0])
         tmax[0] = timeBin* imax
@@ -68,75 +66,113 @@ def read_single_caen(input, output,  iteration, timeBin = 0.4, ):
         tree.Fill()
         i = 0
         #reset voltage array
-        c = array('f', [0.]*1024)
+        #c = array('f', [0.]*1024)
+        #temp = array('f', [0.]*1024)
         evt= evt+1
-        if evt%100==0:
-          print(str(evt)+" events processed")
+        if evt%1000==0:
+          print(str(evt)+" events processed for channel "+str(iteration))
           print("pmax: "+str(pmax[0])+", tmax: "+str(tmax[0])+" baseline correction:"+str(pedestal)+" t0:"+str(t0[0]))
 
        #   print(temp)
       #write tree of time and voltage values to file
+    print ("processed "+str(evt)+" events for channel "+str(iteration)) 
+    f.close()  
     outfile.Write()
-    f.close()
+    outfile.Close()
 
 
-def mergeCAEN( run = '0000'):
-  hdir =  '/Users/patfreeman/Desktop/UCSB/DESY_LGADs/wavedump/CAEN/run'+run+'/'
-
-  out = ROOT.TFile(hdir+'CAENmergerd'+'_'+run+'.root', 'RECREATE')
+def mergeCAEN( run = '0000', hdir='/data/LGADwaveforms/TB/CAEN/' ):
+  dpath = hdir+'Run'+run+'/'
+  out = ROOT.TFile(dpath+'CAENmergerd'+'_'+run+'.root', 'RECREATE')
   
- 
-  for i in range(16):
+  for i in range(17):
     #read the root file
-    f = ROOT.TFile(hdir+'run'+run+'_'+str(i)+'.root', 'READ')
+    f = ROOT.TFile(dpath+'run'+run+'_'+str(i)+'.root', 'READ')
     oldtree = f.Get('tree'+str(i))
     out.cd()
 #    folder = ROOT.TDirectory('c'+str(i),'c'+str(i))
     newtree = oldtree.CloneTree()
     out.Write()
-    print('tree written')
+    print('tree written fo channel'+str(i))
     
-    
-def getTrigID(c, t0, level = 250., timeBin = 0.4):
+def getTrigID(c, t0, level = 350., timeBin = 0.4): #funciton to read bitstream of TLU, 7 bits at 40 MHz (25 ns period) assumed
     trigID = 0
-    for i in range(6): # read 7 bits for now
-       i0 = int( (t0 + 25./2. + 25. *i)/timeBin)
-       bit = round ( statistics.mean(c[i0-2:i0+2])  / level  ) # mean of the center of the bitstream Vpp
+    for i in range(7): # read 7 bits for now
+       i0 = int( (t0 + 25./2. + 25. *i)/timeBin) # int( (t0 + 25./2.)/timeBin) +63*i#
+       if i0 >1021: i0 = 1021
+       print("i0 is "+str(i0)+", the value is "+str(c[i0]) )
+       bit = round ((c[i0-1]+c[i0]+c[i0+1])/3/level) #statistics.mean(c[i0-2:i0+2])  / level  ) # mean of the center of the bitstream Vpp
        trigID = trigID + bit * pow(2, i)
+    print ("the triggerID is:"+str(trigID))
+    return int(trigID)
        
-     
-def addTrigID(merged, trigch, trigIDch):
-
+def addTrigID(merged, trigch, trigIDch, timeBin = 0.4):#function to add trigger IDs to merged trees
+  f = ROOT.TFile(merged, 'READ')
+  out = ROOT.TFile(merged+'.trigID.root', 'RECREATE')
+  for i in range(17):
+    oldtree = f.Get('tree'+str(i))
+    out.cd()
+    tree= oldtree.CloneTree()
+    out.Write()
   trigID = array('i', [0])
 
-  out = ROOT.TFile('trigID'+merged, RECREATE)
-  newtree = ROOT.TTree('treeTrigID')
+  newtree = ROOT.TTree('treeTrigID', 'treeTrigID')
   newtree.Branch("trigID",trigID, "trigID/I")
-  
-  f = ROOT.TFile(merged, 'READ')
   trigTree = f.Get('tree'+str(trigch))
   IDTree = f.Get('tree'+str(trigIDch))
   #get t0 branch from trigger, bitstream begins 25 ns after this
-  for evt in trigTree:
-    t0 = trigTree.Get("t0") + 25
-    c = IDtree.Get("c")
-    trigID = getTrigID(c,t0)
+  nEvt = IDTree.GetEntries()
+  for i in range(nEvt):
+    trigTree.GetEntry(i)
+    t0 = trigTree.t0 + 25
+    IDTree.GetEntry(i)
+    c = IDTree.c
+  #  print(c[640])
+    trigID[0] = int(getTrigID(c,t0, 350., timeBin))
+    print(trigID)
     newtree.Fill()
   out.cd()
-  newtree.Write()
-  trigTree.Write()
-  IDtree.Write()
+  out.Write()
 
-  #copy old trees to new file
+  out.Close()
+  f.Close()
 
 # main part of code, to be executed
-# convert files to root files
-for i in range(16):
-  read_single_caen('/Users/patfreeman/Desktop/UCSB/DESY_LGADs/wavedump/CAEN/run0000/wave_'+str(i)+'.txt', '/Users/patfreeman/Desktop/UCSB/DESY_LGADs/wavedump/CAEN/run0000/run0000_'+str(i)+'.root', i, 0.4)
-  
-mergeCAEN('0000')
+#could do argparsing here for run, path, and time bin
+#parser = argparse.ArgumentParser(description='convert .txt files from CAEN D5742')
+#parser.add_argument('integers', metavar='N', type=int, nargs='+',
+   #                 help='an integer for the accumulator')
 
-addTrigID(CAENmergerd_0000.root, 6, 7 )
+run = '0117'
+dpath = '/data/LGADwaveforms/TB/CAEN/Run'+run+'/'
+timeBin = 0.4
+polarity = [-1,  #polarity of pulses 
+-1,
+-1,
+-1,
+-1,
+-1, 
+-1, 
+1, #triggerID
+-1,
+-1,
+-1,
+-1,
+-1,
+-1,
+-1,
+-1,
+1  #TLU triggerpulse
+]
+
+# convert files to root files 
+
+read_single_caen(dpath+'TR_0_0.txt', dpath+'/run'+run+'_16.root', 16, polarity[16], timeBin) #trigger file
+for i in range(16):
+  read_single_caen(dpath+'wave_'+str(i)+'.txt', dpath+'/run'+run+'_'+str(i)+'.root', i, polarity[i], timeBin)
+
+mergeCAEN(run)
+addTrigID(dpath+'CAENmergerd_'+run+'.root', 16, 7 )
 
 #merge root files, renaming the branch c to c# or putting them in folders
 
